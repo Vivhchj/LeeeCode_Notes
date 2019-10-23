8c9f7be10d70a77fb85bf242ee5219a84bde843a
Jack Hu <641187840@qq.com>
2019-10-23 21:22:25 +0800

    Update 22.括号生成_mid.py

--- 22.括号生成_mid.py
+++ 22.括号生成_mid.py
@@ -48,3 +48,28 @@ class Solution:
                     # str中的每个{}对应后面的参数，此处为'({left}){right}'
                     ans.append('({}){}'.format(left, right))
         return ans
+
+### solution3:动态归划
+### 从0～n，逐渐生成到n组的括号。同样通过'({left}){right}'的形式，生成n组括号时，left+right有(0,n-1)…(i,n-1-i)…(n-1,0)共n-1种情况。
+### 
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return []
+        # 0组括号时记为""，1组括号只有"()"一种情况
+        total_l = [[""], ["()"]]
+        for i in range(2,n+1):    # 开始计算i组括号时的括号组合
+            l = []
+            # 开始遍历 p q ，其中p+q=i-1 , j 作为索引
+            for j in range(i):
+                # p = j 时的括号组合情况
+                now_list1 = total_l[j]
+                # q = (i-1) - j 时的括号组合情况
+                now_list2 = total_l[i-1-j]    
+                # 两部分排列组合
+                for k1 in now_list1:  
+                    for k2 in now_list2:
+                        el = "(" + k1 + ")" + k2
+                        l.append(el)    # 把所有可能的情况添加到 l 中
+            total_l.append(l)    # l这个list就是i组括号的所有情况，添加到total_l中，继续求解i=i+1的情况
+        return total_l[n]
